18cfcabd76c7fc94cb64eb9b6f429ce050ded4d1
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-11-14 13:17:41 -0800

    made note formatting updates

--- active_record_associations.rb
+++ active_record_associations.rb
@@ -1,7 +1,7 @@
-one to one
-  classroom has one teacher
-  one teacher belongs to one classroom
-    put a foreign key on the teachers table
+# one to one ####################################################################################################################################################
+  # classroom has one teacher
+  # one teacher belongs to one classroom
+  #   put a foreign key on the teachers table
       
       has_one, belongs_to
       class Teacher < ApplicationRecord
@@ -11,10 +11,10 @@ one to one
         belongs_to :teacher
       
 
-one to many
-  teacher teaches many courses
-  course belongs to one teacher
-    put a foreign key on the courses table
+# one to many ####################################################################################################################################################
+  # teacher teaches many courses
+  # course belongs to one teacher
+  #   put a foreign key on the courses table
       
       has_many, belongs_to
       class Teacher < ApplicationRecord
@@ -24,20 +24,24 @@ one to many
         belongs_to :teacher  # , optional: true to turn off the validation that there has to be a relationship to courses
 
 
-many to many
-  course has many students and belongs to many students
-  student has many courses and belongs to many courses
-    there is a join table that has two foreign keys in the table (but the join table does not have a model)
+# many to many ####################################################################################################################################################
+#   course has many students and belongs to many students
+#   student has many courses and belongs to many courses
+#     there is a join table that has two foreign keys in the table (but the join table does not have a model)
       
-      has_and_belongs_to_many
-      for when an object has many objectts that belong to it but not exclusively
+      has_and_belongs_to_many       #for when an object has many objectts that belong to it but not exclusively
+      
       class BlogPost < ApplicationRecord
-         has_and_belongs_to_many :categories
+        has_and_belongs_to_many :categories
       class Category < ApplicationRecord
-      Category has_and_belongs_to_many :blogposts
+        has_and_belongs_to_many :blogposts
 
 
-rich join associations
+# rich join associations ####################################################################################################################################################
+#   can do more with these
+#   requires a primary key column (:id)
+#   join table has its own model like '-ments' or '-ships'
+
   class Course < ApplicationRecord
     has_many :course_enrollments
 
@@ -45,7 +49,14 @@ rich join associations
     has_many :course_enrollments
 
   class CourseEnrollment < ApplicationRecord
-    belongs_to :course #has foreign keys
-    belongs_to :student #has foreign keys
+    belongs_to :course #has foreign key
+    belongs_to :student #has foreign key
   
+# traverse rich join associates
+#   students.courses wont work here- theres a join table in between and we need to assign through to fix this
+#   can use student.course_enrollments tho
 
+  # Best practice
+  class Student < ApplicationRecord  
+    has_many :courses, :through => :course_enrollments
+    #now can use student.courses
